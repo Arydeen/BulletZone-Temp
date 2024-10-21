106e5bee1b127308ac4b98fec2caabaaa17a47f2
Kyle Goodwin <kyle.goodwin@unh.edu>
2024-10-21 17:51:24 -0400

    Trying to merge

--- client/src/main/java/edu/unh/cs/cs619/bulletzone/MenuActivity.java
+++ client/src/main/java/edu/unh/cs/cs619/bulletzone/MenuActivity.java
@@ -13,7 +13,9 @@ import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.NonConfigurationInstance;
 
+import edu.unh.cs.cs619.bulletzone.events.GameEventProcessor;
 import edu.unh.cs.cs619.bulletzone.rest.GridPollerTask;
+import edu.unh.cs.cs619.bulletzone.ui.GridAdapter;
 
 @EActivity(R.layout.activity_menu)
 public class MenuActivity extends Activity {
@@ -23,6 +25,12 @@ public class MenuActivity extends Activity {
     @NonConfigurationInstance
     @Bean
     GridPollerTask gridPollTask;
+
+    @Bean
+    protected GridAdapter mGridAdapter;
+
+    @Bean
+    protected GameEventProcessor eventProcessor;
 
     private long userId = -1;
     private long tankId = -1;
@@ -48,6 +56,9 @@ public class MenuActivity extends Activity {
         try {
             tankId = menuController.joinAsync();
             gridPollTask.doPoll();
+            SystemClock.sleep(500); //Wait for poller to update initial board
+            eventProcessor.setBoard(mGridAdapter.getBoard()); //Set initial board to eventprocessor
+            eventProcessor.start(); //Subscribe to eventbus to start posting events
             // Start the Client activity
             Intent intent = new Intent(this, ClientActivity_.class);
             intent.putExtra("USER_ID", userId);
